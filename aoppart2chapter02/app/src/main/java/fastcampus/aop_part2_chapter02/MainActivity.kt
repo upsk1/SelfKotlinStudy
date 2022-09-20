804fa95b17c0da5691e404745f7a71a232e3bdf0
upsk1 <upsk1@naver.com>
2022-09-20 10:27:51 +0900

    Lotto finish

--- aoppart2chapter02/app/src/main/java/fastcampus/aop_part2_chapter02/MainActivity.kt
+++ aoppart2chapter02/app/src/main/java/fastcampus/aop_part2_chapter02/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
@@ -57,11 +58,13 @@ class MainActivity : AppCompatActivity() {
 
             didRun = true
 
-            list.forEachIndexed{index,ab ->
+            list.forEachIndexed{index,number ->
                 val textView = numberTextViewList[index]
 
-                textView.text = ab.toString()
+                textView.text = number.toString()
                 textView.isVisible = true
+
+                setNumberBackground(number,textView);
             }
 
             Log.d("MainActivity", list.toString())
@@ -87,10 +90,21 @@ class MainActivity : AppCompatActivity() {
             textView.isVisible = true
             textView.text = numberPicker.value.toString()
 
+            setNumberBackground(numberPicker.value,textView)
+
             pickNumberSet.add(numberPicker.value)
         }
     }
+    private fun setNumberBackground(number:Int, textView: TextView){
+        when(number){
+            in 1..10 -> textView.background = ContextCompat.getDrawable(this,R.drawable.circle_yello)
+            in 11..20 -> textView.background = ContextCompat.getDrawable(this,R.drawable.circle_blue)
+            in 21..30 -> textView.background = ContextCompat.getDrawable(this,R.drawable.circle_red)
+            in 31..40 -> textView.background = ContextCompat.getDrawable(this,R.drawable.circle_gray)
+            else -> textView.background = ContextCompat.getDrawable(this,R.drawable.circle_green)
 
+        }
+    }
     private fun initClearButton() {
         clearButton.setOnClickListener {
             pickNumberSet.clear()
