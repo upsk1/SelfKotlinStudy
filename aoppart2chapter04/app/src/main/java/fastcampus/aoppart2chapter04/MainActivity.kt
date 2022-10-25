679f9e7820ae14304f21c4b635032de79895fac3
upsk1 <upsk1@naver.com>
2022-10-25 17:12:38 +0900

    finish Calculator UI

---
+++ aoppart2chapter04/app/src/main/java/fastcampus/aoppart2chapter04/MainActivity.kt
@@ -0,0 +1,24 @@
+package fastcampus.aoppart2chapter04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun buttonClicked(v: View){
+
+    }
+    fun resultButtonClicked(v: View){
+
+    }
+    fun historyButtonClicked(v: View){
+
+    }
+    fun clearButtonClicked(v: View){
+
+    }
+}
