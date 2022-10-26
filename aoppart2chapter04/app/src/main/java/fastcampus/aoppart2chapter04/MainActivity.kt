37ff279862c4483f98db47fc38342afc1bcb698d
upsk1 <upsk1@naver.com>
2022-10-26 14:50:55 +0900

    implement clearButton and resultButtonClicked

--- aoppart2chapter04/app/src/main/java/fastcampus/aoppart2chapter04/MainActivity.kt
+++ aoppart2chapter04/app/src/main/java/fastcampus/aoppart2chapter04/MainActivity.kt
@@ -10,10 +10,10 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private val expressionTextView: TextView by lazy{
+    private val expressionTextView: TextView by lazy {
         findViewById<TextView>(R.id.expressionTextView)
     }
-    private val resultTextView: TextView by lazy{
+    private val resultTextView: TextView by lazy {
         findViewById<TextView>(R.id.resultTextView)
     }
     private var isOperator = false
@@ -22,8 +22,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun buttonClicked(v: View){
-        when(v.id){
+
+    fun buttonClicked(v: View) {
+        when (v.id) {
             R.id.button0 -> numberButtonClicked("0")
             R.id.button1 -> numberButtonClicked("1")
             R.id.button2 -> numberButtonClicked("2")
@@ -43,35 +44,37 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun numberButtonClicked(number: String){
-        if(isOperator){
+    private fun numberButtonClicked(number: String) {
+        if (isOperator) {
             expressionTextView.append(" ")
         }
         isOperator = false
 
         val expressionText = expressionTextView.text.split(" ")
-        if(expressionText.isNotEmpty() && expressionText.last().length >= 15){
-            Toast.makeText(this,"15자리 까지만 사용할 수 있습니다.",Toast.LENGTH_SHORT).show()
+        if (expressionText.isNotEmpty() && expressionText.last().length >= 15) {
+            Toast.makeText(this, "15자리 까지만 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
             return
-        } else if (expressionText.last().isEmpty() && number == "0"){
-            Toast.makeText(this,"0은 제일 앞에 올 수 없습니다.",Toast.LENGTH_SHORT).show()
+        } else if (expressionText.last().isEmpty() && number == "0") {
+            Toast.makeText(this, "0은 제일 앞에 올 수 없습니다.", Toast.LENGTH_SHORT).show()
             return
         }
         expressionTextView.append(number)
+        resultTextView.text = calculateExpression()
         // TODO resultTextView 실시간으로 계산 결과를 넣어야하는 기능
     }
-    private fun operatorButtonClicked(operator: String){
-        if(expressionTextView.text.isEmpty()){
+
+    private fun operatorButtonClicked(operator: String) {
+        if (expressionTextView.text.isEmpty()) {
             return
         }
         when {
             isOperator -> {
                 val text = expressionTextView.text.toString()
-                expressionTextView.text = text.dropLast(1)
+                expressionTextView.text = text.dropLast(1) + operator
 
             }
-            hasOperator ->{
-                Toast.makeText(this,"연산자는 한번만 사용할 수 있습니다",Toast.LENGTH_SHORT).show()
+            hasOperator -> {
+                Toast.makeText(this, "연산자는 한번만 사용할 수 있습니다", Toast.LENGTH_SHORT).show()
                 return
             }
             else -> {
@@ -82,21 +85,80 @@ class MainActivity : AppCompatActivity() {
         val ssb = SpannableStringBuilder(expressionTextView.text)
         ssb.setSpan(
             ForegroundColorSpan(getColor(R.color.green)),
-            expressionTextView.text.length-1,
+            expressionTextView.text.length - 1,
             expressionTextView.text.length,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
         expressionTextView.text = ssb
         isOperator = true
         hasOperator = true
     }
 
-    fun resultButtonClicked(v: View){
+    fun resultButtonClicked(v: View) {
+        val expressionTexts = expressionTextView.text.split(" ")
+
+        if (expressionTextView.text.isEmpty() || expressionTexts.size == 1) {
+            return;
+        }
+        if (expressionTexts.size != 3 && hasOperator) {
+            Toast.makeText(this, "아직 완성되지 않은 수식입니다", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (expressionTexts[0].isNumber().not() || expressionTexts[2].isNumber().not()) {
+            Toast.makeText(this, "오류가 발생했습니다", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val expressionText = expressionTextView.text.toString()
+        val resultText = calculateExpression()
+
+        resultTextView.text = ""
+        expressionTextView.text= resultText
+
+        isOperator = false
+        hasOperator = false
+    }
+
+    private fun calculateExpression(): String {
+        val expressionTexts = expressionTextView.text.split(" ")
+        if (hasOperator.not() || expressionTexts.size != 3) {
+            return ""
+        } else if (expressionTexts[0].isNumber().not() || expressionTexts[2].isNumber().not()) {
+            return ""
+        }
+        val exp1 = expressionTexts[0].toBigInteger()
+        val exp2 = expressionTexts[2].toBigInteger()
+        val op = expressionTexts[1]
+
+        return when (op) {
+            "+" -> (exp1 + exp2).toString()
+            "-" -> (exp1 - exp2).toString()
+            "*" -> (exp1 * exp2).toString()
+            "/" -> (exp1 / exp2).toString()
+            "%" -> (exp1 % exp2).toString()
+            else -> ""
+        }
+    }
+
+    fun clearButtonClicked(v: View) {
+        expressionTextView.text = ""
+        resultTextView.text = ""
+        isOperator = false
+        hasOperator = false
+    }
+
+    fun historyButtonClicked(v: View) {
 
     }
-    fun historyButtonClicked(v: View){
 
-    }
-    fun clearButtonClicked(v: View){
+}
 
+// String의 원래 멤버함수인거 처럼 동작한다
+fun String.isNumber(): Boolean {
+    return try {
+        this.toBigInteger()
+        true
+    } catch (e: NumberFormatException) {
+        false
     }
+
 }
